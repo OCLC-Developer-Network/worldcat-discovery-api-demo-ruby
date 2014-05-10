1a8e00c35ed33706d2e8a00bca0b892a12490bd4
meyerst <meyerst@oclc.org>
2014-05-10 13:21:11 -0400

    Too big commit series: converting use WorldCat::Discovery gem - removing configure script

--- config/configure.rb
+++
@@ -1,7 +0,0 @@
-# CONFIG = YAML::load(File.read("#{File.expand_path(File.dirname(__FILE__))}/discovery_api.yml"))
-# 
-# def create_key
-#   key = CONFIG[settings.environment.to_s]['key']
-#   secret = CONFIG[settings.environment.to_s]['secret']
-#   OCLC::Auth::WSKey.new(key, secret)
-# end
