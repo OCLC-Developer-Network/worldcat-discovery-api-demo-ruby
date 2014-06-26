c54c8e3e09ec210737f608ea85ba3390f908811e
Steve Meyer <meyerst@oclc.org>
2014-06-26 13:56:07 -0400

    Updating 'more by this author' functionality to only happen for authors with VIAF URIs

--- public/javascript/discover.js
+++ public/javascript/discover.js
@@ -2,7 +2,10 @@ $(document).ready( function() {
 	
 	$("#more-info").hide();
 	var author = $("#author").attr('resource');
-	$("#more-info").load( "/explore?uri=" + author );
-	$("#more-info").show();
+    if (author != null && author !== undefined && author.match("http://viaf.org/viaf/"))
+    {
+    	$("#more-info").load( "/explore?uri=" + author );
+    	$("#more-info").show();
+    }
 	
 });
