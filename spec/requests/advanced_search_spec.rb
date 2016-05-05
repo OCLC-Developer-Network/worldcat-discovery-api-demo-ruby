cd3fa933c3578ad88f9686cd53f5e921addee853
Karen Coombs <librarywebchic@gmail.com>
2016-05-05 16:06:57 -0500

    Fixed advanced search tests so they pass

--- spec/requests/advanced_search_spec.rb
+++ spec/requests/advanced_search_spec.rb
@@ -16,6 +16,8 @@ require 'spec_helper'
 
 describe "the home page" do
   before do
+    url = 'https://authn.sd00.worldcat.org/oauth2/accessToken?authenticatingInstitutionId=128807&contextInstitutionId=128807&grant_type=client_credentials&scope=WorldCatDiscoveryAPI'
+    stub_request(:post, url).to_return(:body => mock_file_contents("token.json"), :status => 200)
     stub_request(:get, "https://beta.worldcat.org/discovery/database/list").
       to_return(:status => 200, :body => mock_file_contents("database_list.rdf"))
     get '/advanced'
@@ -38,51 +40,56 @@ describe "the home page" do
   end
   
   it "should have a form that submits the correct action" do
-    submit_location = @form_element.attr('action')
-    uri = URI.parse(submit_location)
+    submit_location = @doc.xpath("//form[@id='search-form']/@action")
+    uri = URI.parse(submit_location.first.value)
     expect(uri.path).to eq('/catalog')
   end
   
   it "should have a match dropdown box" do
-    drop_down = @form_element.xpath("//select[@name='operator]")
+    dropdown = @doc.xpath("//select[@name='operator']")
     expect(dropdown).not_to be_empty
     
-    drop_down_values = @form_element.xpath("//select[@name='operator]/option/text()")
+    drop_down_values = @doc.xpath("//select[@name='operator']/option/text()")
     expect(drop_down_values.count).to eq(2)
+    
+    drop_down_values = drop_down_values.map {|drop_down_value| drop_down_value.to_s}
     expect(drop_down_values).to include("All")
     expect(drop_down_values).to include("Any")     
   end
   
   it "should have a keyword input box" do
-    keyword = @form_element.xpath("//input[@name='kw']")
+    keyword = @doc.xpath("//input[@name='kw']")
     expect(keyword).not_to be_empty    
   end
 
   it "should have a name/title input box" do
-    title = @form_element.xpath("//input[@name='name']")
+    title = @doc.xpath("//input[@name='name']")
     expect(title).not_to be_empty    
   end  
 
   it "should have an author input box" do
-    author = @form_element.xpath("//input[@name='creator']")
+    author = @doc.xpath("//input[@name='creator']")
     expect(author).not_to be_empty    
   end  
     
   it "should have a subject input box" do
-    subject = @form_element.xpath("//input[@name='about']")
+    subject = @doc.xpath("//input[@name='about']")
     expect(subject).not_to be_empty    
   end
   
   it "should have scope radio buttons" do
-    scope_values = @form_element.xpath("//input[@name='scope']/@value")
+    scope_values = @doc.xpath("//input[@name='scope']/@value")
     expect(scope_values).not_to be_empty
+    scope_values = scope_values.map {|scope_value| scope_value.value}
     expect(scope_values).to include("my_library")
     expect(scope_values).to include("worldcat")  
   end
   
   
   it "should have a form with the right databases" do
-    databases = @doc.xpath("//p[@id='databases'/label/text()")
+    databases = @doc.xpath("//p[@id='databases']/label/span[@id='database-name']/text()")
+    expect(databases.count).to eq(6)
+    databases = databases.map {|database| database.to_s.gsub("\n", '').squeeze(' ').strip}
     expect(databases).to include("African American Experience")
     expect(databases).to include("American Economic Association Journals")
     expect(databases).to include("OAIster")
