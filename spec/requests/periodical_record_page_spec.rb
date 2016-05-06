d6109437ba58a4552d84e983711e9f646eaa4e5a
Karen Coombs <librarywebchic@gmail.com>
2016-05-06 16:03:00 -0500

    Updated periodical test

--- spec/requests/periodical_record_page_spec.rb
+++ spec/requests/periodical_record_page_spec.rb
@@ -15,11 +15,15 @@
 require 'spec_helper'
 
 describe "the record page" do
+  before(:all) do
+    url = 'https://authn.sd00.worldcat.org/oauth2/accessToken?authenticatingInstitutionId=128807&contextInstitutionId=128807&grant_type=client_credentials&scope=WorldCatDiscoveryAPI'
+    stub_request(:post, url).to_return(:body => mock_file_contents("token.json"), :status => 200)
+  end
   context "when displaying a periodical" do
     before(:all) do
-      stub_request(:get, "https://beta.worldcat.org/discovery/bib/data/2243594").
-        to_return(:status => 200, :body => mock_file_contents("2243594.rdf"))
-      get '/catalog/883876185'
+      stub_request(:get, "https://beta.worldcat.org/discovery/offer/oclc/2243594?heldBy=OCPSB").
+        to_return(:status => 200, :body => mock_file_contents("offer_set_2243594.rdf"))
+      get '/catalog/2243594'
       @doc = Nokogiri::HTML(last_response.body)
     end
     it "should display the title" do
@@ -34,30 +38,34 @@ describe "the record page" do
     
     it "should display the subjects" do
       @subjects = @doc.xpath("//ul[@id='subjects']/li/span/a/text()")
+      expect(@subjects.count).to eq(2)
+      @subjects = @subjects.map {|subject_value| subject_value.to_s}       
       expect(@subjects).to include("Library science")
       expect(@subjects).to include("Academic libraries")
     end
     
     it "should display the genres" do
-      @genres = @doc.xpath("//ul[@id='genres']/li/span/a/text()")
+      @genres = @doc.xpath("//ul[@id='genres']/li/a/text()")
+      expect(@genres.count).to eq(2)
+      @genres = @genres.map {|genres_value| genres_value.to_s} 
       expect(@genres).to include("Periodicals")
       expect(@genres).to include("Electronic journals")
     end
     
     it "should display the format" do
-      #this is busted
       xpath = "//span[@id='format'][text()='Journal/Serial']"
       expect(@doc.xpath(xpath)).not_to be_empty
     end
     
     it "should display the language" do
-      #this is busted
       xpath = "//span[@id='language'][text()='English']"
       expect(@doc.xpath(xpath)).not_to be_empty
     end
     
     it "should display the publication places" do
       @publiciationPlaces = @doc.xpath("//span[@property='library:placeOfPublication']/text()")
+      expect(@publiciationPlaces.count).to eq(2)
+      @publiciationPlaces = @publiciationPlaces.map {|publiciationPlace| publiciationPlace.to_s}
       expect(@publiciationPlaces).to include("New York, etc. :")
     end
     
